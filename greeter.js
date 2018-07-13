fbef40c62fb90cbb0f7bb28118f27f89cef55ff1
Jie Ren Tian <110873204@qq.com>
2018-07-13 14:11:50 +0800

    接口.

--- greeter.js
+++ greeter.js
@@ -1,7 +1,25 @@
 // 类型注解
-function greeter(person) {
-    return 'Hello,' + person;
-}
-var user = "Test";
-user = [1, 2, 3];
-document.body.innerHTML = greeter(user);
+// function greeter(person: string) {
+//     return 'Hello,' + person;
+// }
+// let user = "Test";
+//
+// user = [1,2,3];
+//
+// document.body.innerHTML = greeter(user);
+// 接口的作用
+// interface Person{
+//     firstName: string,
+//     lastName: string,
+// }
+//
+// function greeter(person: Person) {
+//     return 'hello' + person.firstName + person.lastName;
+// }
+//
+// let user = {
+//     firstName: "111",
+//     lastName: "User",
+// };
+//
+// document.body.innerHTML = greeter(user); 
--- greeter.ts
+++ greeter.ts
@@ -1,9 +1,26 @@
 // 类型注解
-function greeter(person: string) {
-    return 'Hello,' + person;
-}
-let user = "Test";
+// function greeter(person: string) {
+//     return 'Hello,' + person;
+// }
+// let user = "Test";
+//
+// user = [1,2,3];
+//
+// document.body.innerHTML = greeter(user);
 
-user = [1,2,3];
-
-document.body.innerHTML = greeter(user);
+// 接口的作用
+// interface Person{
+//     firstName: string,
+//     lastName: string,
+// }
+//
+// function greeter(person: Person) {
+//     return 'hello' + person.firstName + person.lastName;
+// }
+//
+// let user = {
+//     firstName: "111",
+//     lastName: "User",
+// };
+//
+// document.body.innerHTML = greeter(user);
