e4f7ee006fe91820e338edeae102e3d4cbc8bbe9
Jie Ren Tian <110873204@qq.com>
2018-07-13 15:17:15 +0800

    Browserify.Watchify

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -8,29 +8,63 @@
 //     .js.pipe(gulp.dest("dist"));
 // });
 
+// var gulp = require("gulp");
+// var browserify = require("browserify");
+// var source = require("vinyl-source-stream");
+// var tsify = require("tsify");
+// var paths = {
+//   pages: ['src/*.html'],
+// };
+//
+// gulp.task("copy-html", function () {
+//   return gulp.src(paths.pages)
+//     .pipe(gulp.dest("dist"));
+// });
+//
+// gulp.task("default", ["copy-html"], function () {
+//   return browserify({
+//     basedir: '.',
+//     debug: true,
+//     entries: ['src/main.ts'],
+//     cache: {},
+//     packageCache: {}
+//   })
+//     .plugin(tsify)
+//     .bundle()
+//     .pipe(source('bundle.js'))
+//     .pipe(gulp.dest("dist"));
+// });
+
+
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
+var watchify = require("watchify");
 var tsify = require("tsify");
+var gutil = require("gulp-util");
 var paths = {
   pages: ['src/*.html'],
 };
 
+var watchedBrowerify = watchify(browserify({
+  basedir: '.',
+  debug: true,
+  entries: ['src/main.ts'],
+  cache: {},
+  packageCache: {},
+}).plugin(tsify));
+
 gulp.task("copy-html", function () {
-  return gulp.src(paths.pages)
-    .pipe(gulp.dest("dist"));
+  return gulp.src(paths.pages);
 });
 
-gulp.task("default", ["copy-html"], function () {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
-    .plugin(tsify)
+function bundle() {
+  return watchedBrowerify
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest("dist"));
-});
+}
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowerify.on("update", bundle);
+watchedBrowerify.on("log", gutil.log);
