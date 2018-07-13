6458ec08fe05933ca85ad031a508cf7dabd035c5
Jie Ren Tian <110873204@qq.com>
2018-07-13 14:06:53 +0800

    接口的作用.

--- greeter.js
+++ greeter.js
@@ -1,5 +1,7 @@
+// 类型注解
 function greeter(person) {
     return 'Hello,' + person;
 }
 var user = "Test";
+user = [1, 2, 3];
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,9 @@
+// 类型注解
 function greeter(person: string) {
     return 'Hello,' + person;
 }
 let user = "Test";
+
+user = [1,2,3];
 
 document.body.innerHTML = greeter(user);
